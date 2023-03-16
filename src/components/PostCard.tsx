f8980bb2781d753194d0db1f2fcd064795385665
kanaru-ssk <2019january.0131@gmail.com>
2023-03-17 02:49:53 +0900

    fix PostCard style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -21,14 +21,14 @@ const PostCard = ({ post }: Props) => {
             height={144}
             className="h-24 w-32 rounded-l-xl object-cover md:h-48 md:w-64"
           />
-          <article className="relative max-h-max flex-1 p-4">
+          <article className="relative flex-1 px-4 py-2 md:py-4">
             <h3 className="text-md max-h-12 overflow-hidden font-bold md:max-h-14 md:text-xl">
               {post.title}
             </h3>
             <div className="hidden max-h-4 overflow-hidden py-2 text-gray-500 md:block md:max-h-20">
               <p>{post.description}</p>
             </div>
-            <p className="absolute bottom-4 right-4 text-xs text-gray-400">
+            <p className="absolute bottom-2 right-4 text-xs text-gray-400 md:bottom-4">
               {post.createdDate}
             </p>
           </article>
