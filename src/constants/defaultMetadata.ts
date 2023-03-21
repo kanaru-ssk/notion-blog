08c3f1a99342cf099c51a50b3ba7bf695a550b8c
kanaru-ssk <2019january.0131@gmail.com>
2023-03-21 12:03:06 +0900

    rename DOMAIN to NEXT_PUBLIC_DOMAIN

--- src/constants/defaultMetadata.ts
+++ src/constants/defaultMetadata.ts
@@ -1,8 +1,8 @@
 export const defaultMetadata = {
   title: "notion-blog",
   description: "",
-  image: `${process.env.DOMAIN}/ogp.webp`,
-  url: process.env.DOMAIN,
-  icon: `${process.env.DOMAIN}/favicon.svg`,
+  image: `${process.env.NEXT_PUBLIC_DOMAIN}/ogp.webp`,
+  url: process.env.NEXT_PUBLIC_DOMAIN,
+  icon: `${process.env.NEXT_PUBLIC_DOMAIN}/favicon.svg`,
   themeColor: "#fff",
 };
