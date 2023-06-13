931ad311e31f85916887b378ca4e66ecf6adf76c
kanaru-ssk <2019january.0131@gmail.com>
2023-06-13 16:05:12 +0900

    add encodeURI to delete-preview.ts

--- src/pages/api/delete-preview.ts
+++ src/pages/api/delete-preview.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const path = typeof req.query.path === "string" ? req.query.path : "/";
   res.clearPreviewData();
-  return res.redirect(path);
+  return res.redirect(encodeURI(path));
 };
 
 export default handler;
