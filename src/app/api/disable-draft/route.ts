ce691ecd18c359b416cc179a503cc1446bd7017b
kanaru-ssk <2019january.0131@gmail.com>
2023-09-02 16:23:06 +0900

    change function to const

--- src/app/api/disable-draft/route.ts
+++ src/app/api/disable-draft/route.ts
@@ -1,10 +1,10 @@
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function GET(request: Request) {
+export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url);
   const path = searchParams.get("path");
 
   draftMode().disable();
   return redirect(encodeURI(path || "/"));
-}
+};
