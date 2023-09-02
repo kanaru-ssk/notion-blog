f2645a4cc8b99ddb14a8c26ba471c9376931dc4a
kanaru-ssk <2019january.0131@gmail.com>
2023-09-02 12:26:54 +0900

    prettierの設定ファイル修正

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,5 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: ["prettier-plugin-tailwindcss"],
   tailwindConfig: "./tailwind.config.js",
 };
